0bab567a899dd109f33fe937b838830c8e81d111
Romain <hercez92@hotmail.fr>
2022-06-27 00:39:12 +0200

    add username to home

--- src/app/posts/components/home/home.component.ts
+++ src/app/posts/components/home/home.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
-import { AuthService } from '../../../services/auth.service';
+import { FormBuilder } from '@angular/forms';
+import { map, Observable, tap } from 'rxjs';
+import { AuthService } from '../../../auth/services/auth.service';
 
 @Component({
   selector: 'app-home',
@@ -8,19 +10,25 @@ import { AuthService } from '../../../services/auth.service';
 })
 export class HomeComponent implements OnInit {
 
-  constructor() { }
-
+  constructor(
+    private authService: AuthService
+  ) { }
+  username$!: Observable<string>
+  username!: string
   ngOnInit(): void {
+    this.username$ = this.authService.userSession$.pipe(
+      map(userSession => userSession!.username),
+      tap(username => this.username = username)
+    )
   }
-  username = "Romain"
   textContent = "";
+  // urlRegex = /(http(s)?:\/\/.)?(www\.)?[-a-zA-Z0-9@:%._+~#=]{2,256}\.[a-z]{2,6}\b([-a-zA-Z0-9@:%_+.~#?&/=]*)/;
 
   changeText(text: string) {
     this.textContent = text
   }
 
-  postTextContent(e: SubmitEvent) {
-    e.preventDefault()
+  onPostContent() {
     if (!this.textContent) return
     this.textContent = ""
   }
