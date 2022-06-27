132ff2c84260968a6d763d5d87214757b0a8df02
Romain <hercez92@hotmail.fr>
2022-06-27 18:28:30 +0200

    implement new content posting

--- src/app/posts/components/home/home.component.ts
+++ src/app/posts/components/home/home.component.ts
@@ -1,7 +1,8 @@
 import { Component, OnInit } from '@angular/core';
-import { FormBuilder } from '@angular/forms';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { map, Observable, tap } from 'rxjs';
 import { AuthService } from '../../../auth/services/auth.service';
+import { PostsService } from '../../services/posts.service';
 
 @Component({
   selector: 'app-home',
@@ -11,26 +12,31 @@ import { AuthService } from '../../../auth/services/auth.service';
 export class HomeComponent implements OnInit {
 
   constructor(
-    private authService: AuthService
+    private authService: AuthService,
+    private postsService: PostsService,
+    private formBuilder: FormBuilder
   ) { }
   username$!: Observable<string>
   username!: string
+  newPostForm!: FormGroup;
+
   ngOnInit(): void {
     this.username$ = this.authService.userSession$.pipe(
       map(userSession => userSession!.username),
       tap(username => this.username = username)
     )
+    this.newPostForm = this.formBuilder.group({
+      content: ['', Validators.required]
+    })
   }
-  textContent = "";
-  // urlRegex = /(http(s)?:\/\/.)?(www\.)?[-a-zA-Z0-9@:%._+~#=]{2,256}\.[a-z]{2,6}\b([-a-zA-Z0-9@:%_+.~#?&/=]*)/;
 
-  changeText(text: string) {
-    this.textContent = text
-  }
 
   onPostContent() {
-    if (!this.textContent) return
-    this.textContent = ""
+    if (!this.newPostForm.valid) return
+    this.postsService.sendPost(this.newPostForm.value).subscribe(
+      () => this.postsService.getPosts()
+    )
+    this.newPostForm.reset()
   }
 
 }
