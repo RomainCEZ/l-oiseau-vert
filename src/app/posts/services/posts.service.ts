52e20bb59b8bbf7475c62d186dc1d1aff3210191
Romain <hercez92@hotmail.fr>
2022-06-27 18:31:28 +0200

    implement getting 10 posts at a time and infinite scrolling

--- src/app/posts/services/posts.service.ts
+++ src/app/posts/services/posts.service.ts
@@ -1,6 +1,7 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { Observable } from 'rxjs';
+import { tap, Observable, BehaviorSubject } from 'rxjs';
+import { environment } from '../../../environments/environment';
 import { Post } from '../../core/models/Post';
 
 @Injectable({
@@ -10,9 +11,47 @@ export class PostsService {
 
   constructor(private http: HttpClient) { }
 
-  posts: Post[] = []
+  private _loading$ = new BehaviorSubject<boolean>(false)
+  get loading$() {
+    return this._loading$.asObservable()
+  }
 
-  public getPosts(): Observable<Post[]> {
-    return this.http.get<Post[]>('http://localhost:3000/api/posts')
+  private _posts$ = new BehaviorSubject<Post[]>([]);
+  get posts$(): Observable<Post[]> {
+    return this._posts$.asObservable();
+  }
+
+  oldestPost!: Post
+  oldestPostDate: number = Date.now()
+  posts!: Post[]
+
+  private setLoadingStatus(loading: boolean) {
+    this._loading$.next(loading)
+  }
+
+  public getPosts(): void {
+    this.setLoadingStatus(true)
+    this.http.get<Post[]>(`${environment.apiUrl}posts`).pipe(
+      tap(posts => {
+        this.oldestPost = posts[posts.length - 1]
+        this._posts$.next(posts)
+        this.setLoadingStatus(false)
+      })
+    ).subscribe()
+  }
+
+  public getNextTenPosts() {
+    this.setLoadingStatus(!true)
+    this.http.get<Post[]>(`${environment.apiUrl}posts/next/${this.oldestPostDate}`).pipe(
+      tap(posts => {
+        posts.length ? this.oldestPostDate = posts[posts.length - 1].creationDate : posts
+        this._posts$.next(this._posts$.getValue().concat(posts))
+        this.setLoadingStatus(!false)
+      })
+    ).subscribe()
+  }
+
+  public sendPost(post: string): Observable<void> {
+    return this.http.post<void>(`${environment.apiUrl}posts`, post)
   }
 }
