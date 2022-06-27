7f94fec31dace49c2fd3173e9425207ac458cd60
Romain <hercez92@hotmail.fr>
2022-06-27 18:29:42 +0200

    remove resolver

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,12 +4,11 @@ import { HomeComponent } from './posts/components/home/home.component';
 import { LoginComponent } from './auth/login/login.component';
 import { RegisterComponent } from './auth/register/register.component';
 import { AuthGuard } from './core/guards/AuthGuard';
-import { PostsResolver } from './posts/resolvers/posts.resolver';
 
 const routes: Routes = [
   { path: 'login', component: LoginComponent },
   { path: 'register', component: RegisterComponent },
-  { path: '', component: HomeComponent, canActivate: [AuthGuard], resolve: { posts: PostsResolver } }
+  { path: '', component: HomeComponent, canActivate: [AuthGuard] }
 ];
 
 @NgModule({
