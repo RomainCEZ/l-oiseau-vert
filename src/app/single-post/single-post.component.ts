9ac6038bdab123f936634a4ec6f5aa8524d011f7
Romain <hercez92@hotmail.fr>
2022-06-29 05:17:18 +0200

    add edit and delete post

--- src/app/single-post/single-post.component.ts
+++ src/app/single-post/single-post.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { ActivatedRoute } from '@angular/router';
-import { map, Observable, switchMap } from 'rxjs';
+import { tap, Observable, switchMap, map, timeInterval } from 'rxjs';
 import { AuthService } from '../auth/services/auth.service';
 import { Post } from '../core/models/Post';
 import { PostsService } from '../posts/services/posts.service';
@@ -14,18 +15,61 @@ export class SinglePostComponent implements OnInit {
 
   constructor(private route: ActivatedRoute,
     private postsService: PostsService,
-    private authService: AuthService) { }
+    private authService: AuthService, private formBuilder: FormBuilder) { }
 
   post$!: Observable<Post>
   userId$!: Observable<string>
+  editing: boolean = false
+  content!: string
+  editPostForm!: FormGroup
 
   ngOnInit(): void {
-    this.post$ = this.route.params.pipe(
-      switchMap(params => this.postsService.getPostById(params['id']))
-    )
+    this.initObservables()
+  }
+
+  private initObservables(): void {
+    this.getPostById()
     this.userId$ = this.authService.userSession$.pipe(
-      map(userSession => userSession.id)
+      map(userSession => userSession.userId)
     )
   }
 
+  initForm() {
+    this.editPostForm = this.formBuilder.group({ content: [this.content, Validators.required] })
+  }
+
+  private getPostById() {
+    this.post$ = this.route.params.pipe(
+      switchMap(params => this.postsService.getPostById(params['id']))
+    )
+  }
+
+  editPost() {
+    this.post$.subscribe(
+      post => {
+        this.content = post.content
+        this.initForm()
+        this.editing = true
+      }
+    )
+  }
+
+  cancelEdit() {
+    this.editing = false
+  }
+
+  onEditPost() {
+    if (this.content !== this.editPostForm.value.content) {
+      this.post$.subscribe(
+        post => {
+          this.postsService.editPostById(post.id, this.editPostForm.value)
+          this.getPostById()
+        })
+    }
+    this.editing = false
+  }
+
+  deletePost(id: string) {
+    this.postsService.deletePostById(id)
+  }
 }
