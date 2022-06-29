fc7486d9573f901b6be7c189203041c7d057cdfb
Romain <hercez92@hotmail.fr>
2022-06-29 05:17:31 +0200

    add edit and delete post

--- src/app/posts/services/posts.service.ts
+++ src/app/posts/services/posts.service.ts
@@ -1,5 +1,6 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Router } from '@angular/router';
 import { tap, Observable, BehaviorSubject } from 'rxjs';
 import { environment } from '../../../environments/environment';
 import { Post } from '../../core/models/Post';
@@ -9,7 +10,7 @@ import { Post } from '../../core/models/Post';
 })
 export class PostsService {
 
-  constructor(private http: HttpClient) { }
+  constructor(private http: HttpClient, private router: Router) { }
 
   private _loading$ = new BehaviorSubject<boolean>(false)
   get loading$() {
@@ -44,7 +45,7 @@ export class PostsService {
     this.setLoadingStatus(true)
     this.http.get<Post[]>(`${environment.apiUrl}posts/next/${this.oldestPostDate}`).pipe(
       tap(posts => {
-        posts.length ? this.oldestPostDate = posts[posts.length - 1].creationDate : posts
+        posts.length ? this.oldestPostDate = posts[posts.length - 1].timestamp : posts
         this._posts$.next(this._posts$.getValue().concat(posts))
         this.setLoadingStatus(false)
       })
@@ -57,5 +58,21 @@ export class PostsService {
 
   public getPostById(id: string): Observable<Post> {
     return this.http.get<Post>(`${environment.apiUrl}posts/${id}`)
+  }
+
+  public editPostById(id: string, content: string): void {
+    this.http.put<void>(`${environment.apiUrl}posts/${id}`, content)
+      .subscribe(() => {
+        this.router.navigateByUrl(`post/${id}`)
+        this.getPosts()
+      })
+  }
+
+  public deletePostById(id: string): void {
+    this.http.delete<void>(`${environment.apiUrl}posts/${id}`)
+      .subscribe(() => {
+        this.router.navigateByUrl("/")
+        this.getPosts()
+      })
   }
 }
